b2cbffc3e9b35944f6722ac51d432c3148e5cf7f
Brody Macfarlane <macfarlanebrody@gmail.com>
2017-10-23 14:27:53 -0600

    structure

---
+++ responsive-site-clone/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="navbar">
+          <div className="logo cursive-font">Boot Startstrap</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
