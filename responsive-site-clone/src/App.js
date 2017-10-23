cf66611e2e77c30fbc0d4141ee3fac61e1175305
Brody Macfarlane <macfarlanebrody@gmail.com>
2017-10-23 14:57:01 -0600

    collab time

--- responsive-site-clone/src/App.js
+++ responsive-site-clone/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import './font-awesome/css/font-awesome.css';
 
 class App extends Component {
   render() {
@@ -8,6 +8,9 @@ class App extends Component {
       <div className="App">
         <div className="navbar">
           <div className="logo cursive-font">Boot Startstrap</div>
+          <div className="menu-button">
+            MENU<i className="fa fa-bars"></i>
+          </div>
         </div>
       </div>
     );
