50c13570e819ab5aa28d02e68862889b87c8c9c6
Nick Hathaway <Nicholashathaway@gmail.com>
2017-10-23 16:46:52 -0600

    bacground image, call to action, and breakpoint for nav links

--- responsive-site-clone/src/App.js
+++ responsive-site-clone/src/App.js
@@ -6,12 +6,26 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="navbar">
-          <div className="navbar-content">
-            <div className="logo cursive-font">Boot Startstrap</div>
-            <div className="menu-button">
-              MENU<i className="fa fa-bars"></i>
+        <div className="landing">
+          <div className="navbar">
+            <div className="navbar-content">
+              <div className="logo cursive-font">Boot Startstrap</div>
+              <div className="links">
+                <div>SERVICES</div>
+                <div>PORTFOLIO</div>
+                <div>ABOUT</div>
+                <div>TEAM</div>
+                <div>CONTACT</div>
+              </div>
+              <div className="menu-button">
+                MENU<i className="fa fa-bars"></i>
+              </div>
             </div>
+          </div>
+          <div className="cta-content">
+            <h2>Welcome To Our Studio!</h2>
+            <h1>IT'S NICE TO MEET YOU</h1>
+            <button>TELL ME MORE</button>
           </div>
         </div>
       </div>
